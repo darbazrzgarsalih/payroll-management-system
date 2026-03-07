b0b45247099b5bd6244e8a87b71022e347cd8383
darbo-3 <darbomicr@gmail.com>
2026-03-07 03:11:57 +0300

    Fix: Support Authorization header for mobile device compatibility

--- backend/source/middlewares/Auth.Middleware.js
+++ backend/source/middlewares/Auth.Middleware.js
@@ -7,7 +7,12 @@ const jwtSecret = String(process.env.JWT_SECRET)
 
 export const authMiddleware = async (req, res, next) => {
     try {
-        const token = req.cookies?.token;
+        let token = req.cookies?.token;
+
+        if (!token && req.headers.authorization?.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1];
+        }
+
         if (!token) {
             return next(new UnauthorizedError('No token found, access denied'));
         }
